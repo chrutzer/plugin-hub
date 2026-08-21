615039c067c5a2d13df44b7daff657e8cf7e3e93
Christian <chrigi.rutzer@hotmail.com>
2026-08-21 09:58:16 +0200

    add cors

--- internal/api/api.go
+++ internal/api/api.go
@@ -24,7 +24,20 @@ func New(reg *registry.Registry) *Server {
 }
 
 func (s *Server) Handler() http.Handler {
-	return s.mux
+	return corsMiddleware(s.mux)
+}
+
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
 
 type pluginSummary struct {
